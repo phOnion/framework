4bbabe133d2e1fed2e4de0be708df3757813f3a1
Dimitar Dimitrov <daghostman.dimitrov@gmail.com>
2016-08-25 02:37:59 +0100

    Revert 6c6ec0b7064e3b61f7d3db9743ca5bfbb5148c31 that removes the interface (fixes test failures)

---
+++ src/Framework/Interfaces/Middleware/MiddlewareInterface.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * PHP Version 5.6.0
+ *
+ * @category Unknown Category
+ * @package  Onion\Framework\Interfaces\Middleware
+ * @author   Dimitar Dimitrov <daghostman.dd@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/phOnion/framework
+ */
+
+namespace Onion\Framework\Interfaces\Middleware;
+
+use Psr\Http\Message;
+
+/**
+ * Interface MiddlewareInterface
+ *
+ * @package Onion\Framework\Interfaces\Middleware
+ */
+interface MiddlewareInterface
+{
+    /**
+     * @param Message\RequestInterface $request
+     * @param FrameInterface           $frame
+     *
+     * @return Message\ResponseInterface
+     */
+    public function process(Message\RequestInterface $request, FrameInterface $frame = null);
+}
