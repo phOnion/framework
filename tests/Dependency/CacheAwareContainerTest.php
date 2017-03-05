3de965a997fb35781b0a616fb3c9701c94d6add8
Dimitar Dimitrov <daghostman.dd@gmail.com>
2017-03-05 18:27:21 +0200

    Introduce container capable of caching the dependencies (#26)
    * Introduce cache aware container

---
+++ tests/Dependency/CacheAwareContainerTest.php
@@ -0,0 +1,93 @@
+<?php
+namespace tests\Dependency;
+
+use Onion\Framework\Dependency\CacheAwareContainer;
+use Onion\Framework\Dependency\Interfaces\FactoryInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface as Container;
+use Psr\SimpleCache\CacheInterface;
+
+class CacheAwareContainerTest extends \PHPUnit_Framework_TestCase
+{
+    private $cache;
+    private $factory;
+    public function setUp()
+    {
+        $this->cache = $this->prophesize(CacheInterface::class);
+        $this->factory = new class implements FactoryInterface
+        {
+            public function build(Container $container)
+            {
+                return new \Onion\Framework\Dependency\Container([
+                    'bar' => 'baz'
+                ]);
+            }
+        };
+    }
+
+    public function testCacheNotHavingKey()
+    {
+        $this->cache->has('test')->willReturn(false);
+        $cacheContainer = new CacheAwareContainer(
+            $this->factory,
+            $this->cache->reveal()
+        );
+
+        $this->assertFalse($cacheContainer->has('test'));
+    }
+
+    public function testCacheHavingKey()
+    {
+        $this->cache->has('test')->willReturn(true);
+        $cacheContainer = new CacheAwareContainer(
+            $this->factory,
+            $this->cache->reveal()
+        );
+
+        $this->assertTrue($cacheContainer->has('test'));
+    }
+
+    public function testCacheRetrievalOfKey()
+    {
+        $this->cache->has('test')->willReturn(false);
+        $this->cache->has('foo')->willReturn(true);
+        $this->cache->get('foo')->willReturn('bar');
+
+        $cacheContainer = new CacheAwareContainer(
+            $this->factory,
+            $this->cache->reveal()
+        );
+
+        $this->assertFalse($cacheContainer->has('test'));
+        $this->assertTrue($cacheContainer->has('foo'));
+        $this->assertSame($cacheContainer->get('foo'), 'bar');
+
+    }
+
+    public function testStoringValueInCache()
+    {
+        $this->cache->has('bar')->willReturn(false);
+        $this->cache->set('bar', 'baz')->willReturn(true);
+
+        $cacheContainer = new CacheAwareContainer(
+            $this->factory,
+            $this->cache->reveal()
+        );
+        $this->assertTrue($cacheContainer->has('bar'));
+        $this->assertSame($cacheContainer->get('bar'), 'baz');
+    }
+
+    public function testExceptionWhenStoringValueInCacheFails()
+    {
+        $this->cache->has('bar')->willReturn(false);
+        $this->cache->set('bar', 'baz')->willReturn(false);
+
+        $cacheContainer = new CacheAwareContainer(
+            $this->factory,
+            $this->cache->reveal()
+        );
+        $this->assertTrue($cacheContainer->has('bar'));
+        $this->expectException(ContainerExceptionInterface::class);
+        $cacheContainer->get('bar');
+    }
+}
