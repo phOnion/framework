f207a90c1ff48f37a9871211bf5951c96b0f8f44
Dimitar Dimitrov <daghostman.dd@gmail.com>
2018-04-15 00:56:27 +0300

    Remove obsolete reference to RouterInterface

--- src/Framework/Http/Middleware/Factory/RequestHandlerFactory.php
+++ src/Framework/Http/Middleware/Factory/RequestHandlerFactory.php
@@ -3,20 +3,11 @@ namespace Onion\Framework\Http\Middleware\Factory;
 
 use Onion\Framework\Router\Route;
 use Psr\Container\ContainerInterface;
-use Onion\Framework\Router\Interfaces\RouterInterface;
 use Onion\Framework\Dependency\Interfaces\FactoryInterface;
 use Onion\Framework\Http\Middleware\RequestHandler;
 
 class RequestHandlerFactory implements FactoryInterface
 {
-    /** @var Router */
-    private $router;
-
-    public function __construct(RouterInterface $router)
-    {
-        $this->router = $router;
-    }
-
     public function build(ContainerInterface $container)
     {
         if (!$container->has('middleware')) {
