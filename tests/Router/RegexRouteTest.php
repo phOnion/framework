690d9587fe5a08e05895ec262c121949531504bb
Dimitar Dimitrov <daghostman.dd@gmail.com>
2018-04-15 00:18:35 +0300

    Add test for RegexRoutes

---
+++ tests/Router/RegexRouteTest.php
@@ -0,0 +1,92 @@
+<?php declare(strict_types=1);
+
+namespace Test\Router;
+
+use Psr\Http\Message\UriInterface;
+use Onion\Framework\Router\RegexRoute;
+use Psr\Http\Message\ResponseInterface;
+use Prophecy\Argument\Token\AnyValueToken;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class RegexRouteTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var RegexRoute */
+    private $route;
+    public function setUp()
+    {
+        $this->route = new RegexRoute('/');
+    }
+
+    public function testRouteName()
+    {
+        $this->assertSame('/', $this->route->getName());
+    }
+
+    public function testRouteGetPattern()
+    {
+        $this->assertSame('/', $this->route->getPattern());
+    }
+
+    public function testPatternHandling()
+    {
+        $route = new RegexRoute('/[test][/[bar]]');
+        $this->assertSame('/[test][/[bar]]', $route->getName());
+        $this->assertSame('/[test][/[bar]]', $route->getPattern());
+        $this->assertTrue($route->isMatch('/foo'));
+        $this->assertSame(['test' => 'foo'], $route->getParameters());
+        $this->assertTrue($route->isMatch('/foo/baz'));
+        $this->assertSame(['test' => 'foo', 'bar' => 'baz'], $route->getParameters());
+    }
+
+    public function testRouteRequestHandler()
+    {
+        $headers = [
+            'content-type' => 'text/plain',
+        ];
+
+
+        $response = $this->prophesize(ResponseInterface::class);
+        $response->withAddedHeader('content-type', 'text/plain')
+            ->willReturn($response->reveal());
+        $response->withAddedHeader('Access-Control-Allow-Methods', 'GET')
+            ->willReturn($response->reveal());
+        $response->withAddedHeader('Access-Control-Max-Age', '86400')
+            ->willReturn($response->reveal());
+        $response->withAddedHeader('Access-Control-Allow-Origin', '*')
+            ->willReturn($response->reveal());
+        $response->withAddedHeader('Access-Control-Allow-Credentials', 'true')
+            ->willReturn($response->reveal());
+        $response->hasHeader('Access-Control-Allow-Origin')
+            ->willReturn(false);
+
+        $uri = $this->prophesize(UriInterface::class);
+        $uri->getAuthority()->willReturn('');
+
+        $request = $this->prophesize(ServerRequestInterface::class);
+        $request->getUri()->willReturn($uri->reveal());
+
+        $handler = $this->prophesize(RequestHandlerInterface::class);
+        $handler->handle(new AnyValueToken())->willReturn($response->reveal());
+
+        $route = $this->route->withRequestHandler($handler->reveal())
+            ->withHeaders(new \ArrayIterator($headers))
+            ->withMethods(['GET']);
+        $this->assertNotSame($this->route, $route);
+
+        $this->assertInstanceOf(
+            ResponseInterface::class,
+            $route->handle($request->reveal())
+        );
+    }
+
+    public function testRouteMethods()
+    {
+        $route = $this->route->withMethods(new \ArrayIterator(['GET', 'HEAD']));
+        $this->assertNotSame($this->route, $route);
+        $this->assertTrue($route->hasMethod('GET'));
+        $this->assertTrue($route->hasMethod('HEAD'));
+        $this->assertFalse($route->hasMethod('POST'));
+        $this->assertSame(['GET', 'HEAD'], $route->getMethods());
+    }
+}
