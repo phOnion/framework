9466c06806e2665830d4307258e9b673f0299b2c
DaGhostman <daghostman.dd@gmail.com>
2019-05-20 17:55:44 +0300

    add state builder factory

---
+++ src/State/Factory/FlowFactory.php
@@ -0,0 +1,37 @@
+<?php
+namespace Onion\Framework\State\Factory;
+
+use Onion\Framework\Dependency\Interfaces\FactoryBuilderInterface;
+use Onion\Framework\Dependency\Interfaces\FactoryInterface;
+use Psr\Container\ContainerInterface;
+use Onion\Framework\Common\Config\Container;
+use Onion\Framework\State\Flow;
+use Onion\Framework\State\Transition;
+
+class FlowFactory implements FactoryBuilderInterface
+{
+    public function build(ContainerInterface $container, string $key): FactoryInterface
+    {
+        return new class ($key, $container->get("states.{$key}")) implements FactoryInterface
+        {
+            private $name;
+            private $states;
+            public function __construct(string $name, Container $states)
+            {
+                $this->name = $name;
+                $this->states = $states;
+            }
+
+            public function build(\Psr\Container\ContainerInterface $container)
+            {
+                $flow = new Flow($this->name, $this->states->get('initial'));
+
+                foreach ($this->states->get('transitions') as $def) {
+                    $flow->addTransition(new Transition($def['source'], $def['destination'], $def['handler'] ?? null));
+                }
+
+                return $flow;
+            }
+        };
+    }
+}
