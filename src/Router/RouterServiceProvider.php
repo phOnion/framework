f923050812515cc062b64efbad95689a16c3aea2
Dimitar Dimitrov <daghostman.dd@gmail.com>
2022-05-17 21:27:37 +0300

    introduce router service provider

---
+++ src/Router/RouterServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Onion\Framework\Router;
+
+use Onion\Framework\Dependency\Interfaces\ContainerInterface;
+use Onion\Framework\Dependency\Interfaces\ServiceProviderInterface;
+use Onion\Framework\Router\Interfaces\ResolverInterface;
+use Onion\Framework\Router\Strategy\Factory\TreeStrategyFactory;
+
+class RouterServiceProvider implements ServiceProviderInterface
+{
+
+    public function register(ContainerInterface $provider): void
+    {
+        $provider->bind(ResolverInterface::class, TreeStrategyFactory::class);
+    }
+}
