51437f50203fc5139f1c4fb4205fd917c9ed65fe
Dimitar Dimitrov <daghostman.dd@gmail.com>
2023-04-02 23:45:22 +0300

    fix(psalm): add missing typehints

--- src/State/History.php
+++ src/State/History.php
@@ -6,8 +6,14 @@ namespace Onion\Framework\State;
 
 use Onion\Framework\State\Interfaces\HistoryInterface;
 
+/**
+ * @template-implements \IteratorAggregate<int, array{string, string, array<mixed>}>
+ */
 class History implements \IteratorAggregate, HistoryInterface, \Countable
 {
+    /**
+     * @var array<int, array{string, string, array<mixed>}>
+     */
     private $transitions = [];
 
     public function add(string $from, string $to, array $arguments): void
